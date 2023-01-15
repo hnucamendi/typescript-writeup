dae014d0f4f359929c257ea4c82bee627478f820
Harold Arriola Nucamendi <hnucamend@hnucamendi.attlocal.net>
2023-01-15 01:29:22 -0500

    cleanup Person problem

--- person/person.ts
+++ person/person.ts
@@ -1,14 +1,20 @@
-// ## **Person Problem**
+// **Person Problem**
 
-// 4. Create a class for a person that has properties for the person's name, age, and address.
+// Create a class for a person that has properties for the person's name, age, and address.
 // The class should have a method that takes a string as an argument and returns the person's name and address concatenated together.
-//  Use type annotations for the properties and method, and make sure to catch any errors that may occur when calling the method.
+// Use type annotations for the properties and method, and make sure to catch any errors that may occur when calling the method.
 
 class Person {
   name: string;
   age: number;
   address: string;
 
+  /**
+   *
+   * @param name
+   * @param age
+   * @param address
+   */
   constructor(name: string, age: number, address: string) {
     if (!name || age < 0 || !address) {
       throw new Error(
@@ -20,6 +26,10 @@ class Person {
     this.address = address;
   }
 
+  /**
+   *
+   * @returns {string} Returns interpolated string with info of the Person instance
+   */
   printInfo(): string {
     return `${this.name} is ${this.age} years old. ${this.name}'s current address is: ${this.address}`;
   }
